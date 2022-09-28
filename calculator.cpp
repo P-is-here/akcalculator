480254398a1b208c90394b1fa534283d191c4cf0
Pavlo Nasonov <pasha.nasonovhtc@gmail.com>
2022-09-28 23:31:06 +0300

    my changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,20 @@
 #include "calculator.h"
 
+//good functions 
+
+//adding of two numbers
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//substracting of variables
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//product of two numbers adding 0.5
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
